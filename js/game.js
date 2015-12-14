615bb04f005c1ab6174441b59d401d967ba907b5
AdventureOwl <tomhood@Toms-MacBook-Pro.local>
2015-12-14 10:46:28 +1000

    size

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(960, 540, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
     game.load.image('player', 'img/square.png');
