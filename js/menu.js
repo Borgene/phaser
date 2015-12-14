7f57b4a36d8f8dca789d8db86860f7b33f7dd487
AdventureOwl <tomhood@Toms-MacBook-Pro.local>
2015-12-14 11:34:25 +1000

    menu

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ var Menu = {
 
     preload : function() {
         // Load all the needed resources for the menu.
-        game.load.image('menu', './img/background.png');
+        game.load.image('menu', './img/menu.png');
     },
 
     create: function () {
